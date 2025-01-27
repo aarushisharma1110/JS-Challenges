699d6ba8041a8a68a6bbe4b475b4db5b54bc0c3c
aarushisharma1110 <aarushisharma1110@gmail.com>
2025-01-26 20:38:15 -0800

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,8 @@
+//Function to output a console message and create an alert popup.
+function sayHello(){
+     console.log("Hello world!");
+     alert("hello world!");
+  }       
+
+//Call the function sayHello()
+sayHello():
